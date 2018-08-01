83b9af13cb82e2faa4371cf49ee3394d8c900c7a
Alejandro <x238922@wnco.com>
2018-08-01 18:31:52 -0500

    Initial Commit

---
+++ src/main/java/com/escaladaeveryday/application/EscaladaEverydaySpringApplication.java
@@ -0,0 +1,13 @@
+package com.escaladaeveryday.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscaladaEverydaySpringApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(EscaladaEverydaySpringApplication.class, args);
+   }
+
+}
