b30ff07a8703491ef3e9aa03821a3c0bc6faddd3
ANLevant <Azakura1213>
2018-08-05 19:07:04 -0500

    [ANLevant] Add new Angular behaviour in accordance to tour of heroes tutorial

--- src/main/angular/EscaladaEveryday/src/app/app.component.ts
+++ src/main/angular/EscaladaEveryday/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EscaladaEveryday';
+  title = 'Escalada Everyday';
 }
--- src/main/angular/EscaladaEveryday/src/app/app.module.ts
+++ src/main/angular/EscaladaEveryday/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserManagementComponent,
+    UserDetailComponent,
+    MessagesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
